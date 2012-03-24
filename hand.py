b386ee4564465b3b546e2aaa733dbdabf3b698ec
Doug von Kohorn <dougvk@gmail.com>
2012-03-23 23:34:01 -0700

    updated hand with more helper functions and tests

--- hand.py
+++ hand.py
@@ -8,20 +8,30 @@ class Hand():
         self.hand = [first, second]
         self.dealer = dealer
 
+    def __str__(self):
+        return "The Hand: %s" % self.hand
+
     def add(self, card):
         self.hand.append(card)
     
     def hard_total(self):
         return sum(map(lambda x: x.rank, self.hand))
 
-    def soft_total(self):
-        hard_total = map(lambda x: x.rank, self.hand)
-        if 1 in hard_total:
-            soft_total = sum(hard_total) + 10
-            return soft_total if soft_total <= 21 else hard_total
+    def total(self):
+        hard_hand = map(lambda x: x.rank, self.hand)
+        if 1 in hard_hand:
+            soft_total = sum(hard_hand) + 10
+            return soft_total if soft_total <= 21 else sum(hard_hand)
+        return sum(hard_hand)
     
-    def upcard(self)
+    def upcard(self):
         return self.hand[0].rank
+
+    def blackjack(self):
+        return len(self.hand) == 2 and self.total() == 21
+
+    def bust(self):
+        return self.total() > 21
 
 class TestHand(unittest.TestCase):
     def setUp(self):
@@ -30,72 +40,57 @@ class TestHand(unittest.TestCase):
         self.acecard = BlackjackCard(BlackjackCard.ace)
 
         self.hand_player = Hand(self.upcard, self.downcard, False)
-        self.hand_dealer = Hand(self.upcard, self.downcard, True)
 
         self.ace_player = Hand(self.acecard, self.downcard, False)
-        self.ace_dealer = Hand(self.acecard, self.downcard, True)
 
         self.two_ace_player = Hand(self.acecard, self.downcard, False)
-        self.two_ace_dealer = Hand(self.acecard, self.downcard, True)
         self.two_ace_player.add(self.acecard)
-        self.two_ace_dealer.add(self.acecard)
 
         self.three_ace_player = Hand(self.acecard, self.downcard, False)
-        self.three_ace_dealer = Hand(self.acecard, self.downcard, True)
         self.three_ace_player.add(self.acecard)
-        self.three_ace_dealer.add(self.acecard)
         self.three_ace_player.add(self.acecard)
-        self.three_ace_dealer.add(self.acecard)
 
         self.bust_player = Hand(self.upcard, self.downcard, False)
-        self.bust_dealer = Hand(self.upcard, self.downcard, True)
         self.bust_player.add(self.upcard)
-        self.bust_dealer.add(self.upcard)
 
         self.bust_ace_player = Hand(self.upcard, self.downcard, False)
-        self.bust_ace_dealer = Hand(self.upcard, self.downcard, True)
         self.bust_ace_player.add(self.upcard)
-        self.bust_ace_dealer.add(self.upcard)
         self.bust_ace_player.add(self.acecard)
-        self.bust_ace_dealer.add(self.acecard)
+
+        self.blackjack_player = Hand(self.upcard, self.acecard, False)
 
     def test_hard_value(self):
         self.assertEqual(self.hand_player.hard_total(), 12)
-        self.assertEqual(self.hand_dealer.hard_total(), 12)
-
         self.assertEqual(self.ace_player.hard_total(), 3)
-        self.assertEqual(self.ace_dealer.hard_total(), 3)
-
         self.assertEqual(self.two_ace_player.hard_total(), 4)
-        self.assertEqual(self.two_ace_dealer.hard_total(), 4)
-
         self.assertEqual(self.three_ace_player.hard_total(), 5)
-        self.assertEqual(self.three_ace_dealer.hard_total(), 5)
-
         self.assertEqual(self.bust_player.hard_total(), 22)
-        self.assertEqual(self.bust_dealer.hard_total(), 22)
-
         self.assertEqual(self.bust_ace_player.hard_total(), 23)
-        self.assertEqual(self.bust_ace_dealer.hard_total(), 23)
 
     def test_soft_value(self):
-        self.assertEqual(self.hand_player.hard_total(), 12)
-        self.assertEqual(self.hand_dealer.hard_total(), 12)
+        self.assertEqual(self.hand_player.total(), 12)
+        self.assertEqual(self.ace_player.total(), 13)
+        self.assertEqual(self.two_ace_player.total(), 14)
+        self.assertEqual(self.three_ace_player.total(), 15)
+        self.assertEqual(self.bust_player.total(), 22)
+        self.assertEqual(self.bust_ace_player.total(), 23)
 
-        self.assertEqual(self.ace_player.soft_total(), 13)
-        self.assertEqual(self.ace_dealer.soft_total(), 13)
+    def test_busted(self):
+        self.assertEqual(True, self.bust_player.bust())
+        self.assertEqual(True, self.bust_ace_player.bust())
+        self.assertEqual(False, self.hand_player.bust())
+        self.assertEqual(False, self.ace_player.bust())
+        self.assertEqual(False, self.two_ace_player.bust())
+        self.assertEqual(False, self.three_ace_player.bust())
 
-        self.assertEqual(self.two_ace_player.soft_total(), 14)
-        self.assertEqual(self.two_ace_dealer.soft_total(), 14)
-
-        self.assertEqual(self.three_ace_player.soft_total(), 15)
-        self.assertEqual(self.three_ace_dealer.soft_total(), 15)
-
-        self.assertEqual(self.bust_player.hard_total(), 22)
-        self.assertEqual(self.bust_dealer.hard_total(), 22)
-
-        self.assertEqual(self.bust_ace_player.hard_total(), 23)
-        self.assertEqual(self.bust_ace_dealer.hard_total(), 23)
+    def test_blackjack(self):
+        self.assertEqual(False, self.bust_player.blackjack())
+        self.assertEqual(False, self.bust_ace_player.blackjack())
+        self.assertEqual(False, self.hand_player.blackjack())
+        self.assertEqual(False, self.ace_player.blackjack())
+        self.assertEqual(False, self.two_ace_player.blackjack())
+        self.assertEqual(False, self.three_ace_player.blackjack())
+        self.assertEqual(True, self.blackjack_player.blackjack())
 
 if __name__ == "__main__":
     unittest.main()
